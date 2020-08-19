e3382d1265e4cbbc594b992d3d2b61d3376b4361
Tony Silva <tony.music7@gmail.com>
2020-08-19 10:26:26 -0300

    Primeira Classe: Position

--- src/application12/Program12.java
+++ src/application12/Program12.java
@@ -1,9 +1,12 @@
 package application12;
 
+import boardgame.Position;
+
 public class Program12 {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		Position pos = new Position(3, 5);
+		System.out.println(pos);
 	}
 
 }
