f685029a1982ddecf9f7721ec155394d2aef6928
Tony Silva <tony.music7@gmail.com>
2020-08-30 09:58:06 -0300

    Rainha

--- src/chess/pieces/Rook.java
+++ src/chess/pieces/Rook.java
@@ -68,6 +68,53 @@ public class Rook extends ChessPiece {
 		if (getBoard().positionExists(aux) && isThereOpponentPiece(aux)) {
 			mat[aux.getRow()][aux.getColumn()] = true; // marca como posição válida
 		}
+		
+		// testa posição NOROESTE da peça ............................................
+		aux.setValues(position.getRow() - 1, position.getColumn() - 1);
+		// enquanto existir posição pra aux e não estiver ocupada
+		while (getBoard().positionExists(aux) && !getBoard().thereIsAPiece(aux)) {
+			mat[aux.getRow()][aux.getColumn()] = true; // marca como posição válida
+			aux.setValues(aux.getRow() - 1, aux.getColumn() - 1); // noroeste
+		}
+		// se existir posição pra aux e estiver ocupada por peça oponente...
+		if (getBoard().positionExists(aux) && isThereOpponentPiece(aux)) {
+			mat[aux.getRow()][aux.getColumn()] = true; // marca como posição válida
+		}
+
+		// testa posição A NORDESTE da peça .........................................
+		aux.setValues(position.getRow() - 1, position.getColumn() + 1);
+		// enquanto existir posição pra aux e não estiver ocupada
+		while (getBoard().positionExists(aux) && !getBoard().thereIsAPiece(aux)) {
+			mat[aux.getRow()][aux.getColumn()] = true; // marca como posição válida
+			aux.setValues(aux.getRow() - 1, aux.getColumn() + 1); // nordeste
+		}
+		// se existir posição pra aux e estiver ocupada por peça oponente...
+		if (getBoard().positionExists(aux) && isThereOpponentPiece(aux)) {
+			mat[aux.getRow()][aux.getColumn()] = true; // marca como posição válida
+		}
+		// testa posição A SUDESTE da peça .........................................
+		aux.setValues(position.getRow() + 1, position.getColumn() + 1);
+		// enquanto existir posição pra aux e não estiver ocupada
+		while (getBoard().positionExists(aux) && !getBoard().thereIsAPiece(aux)) {
+			mat[aux.getRow()][aux.getColumn()] = true; // marca como posição válida
+			aux.setValues(aux.getRow() + 1, aux.getColumn() + 1);
+		}
+		// se existir posição pra aux e estiver ocupada por peça oponente...
+		if (getBoard().positionExists(aux) && isThereOpponentPiece(aux)) {
+			mat[aux.getRow()][aux.getColumn()] = true; // marca como posição válida
+		}
+
+		// testa posição SUDOESTE da peça ............................................
+		aux.setValues(position.getRow() + 1, position.getColumn() - 1);
+		// enquanto existir posição pra aux e não estiver ocupada
+		while (getBoard().positionExists(aux) && !getBoard().thereIsAPiece(aux)) {
+			mat[aux.getRow()][aux.getColumn()] = true; // marca como posição válida
+			aux.setValues(aux.getRow() + 1, aux.getColumn() - 1);
+		}
+		// se existir posição pra aux e estiver ocupada por peça oponente...
+		if (getBoard().positionExists(aux) && isThereOpponentPiece(aux)) {
+			mat[aux.getRow()][aux.getColumn()] = true; // marca como posição válida
+		}
 
 		return mat;
 		// NOTA. A Torre pode mover apenas para frente e lados
